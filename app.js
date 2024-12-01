9635a4ed8b37a3698a7f26f414b86284ec58b264
Ganesh <ganeshsudhanagunta68@gmail.com>
2024-12-01 15:56:47 +0530

    Database Updated

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const createTableIfNotExists = async () => {
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             address VARCHAR(255),
-            latitude DOUBLE,
-            longitude DOUBLE
+            latitude FLOAT,
+            longitude FLOAT
         );
     `;
 
